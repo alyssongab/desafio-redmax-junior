b9adb2ade1fd2132717f4e9caaacc75de9f7207e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-08 22:35:56 -0400

    feat: adiciona o controller de usuarios

---
+++ backend/src/modules/usuarios/usuario.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import * as service from "./usuario.service.js";
+
+export const createUser = async(req: Request, res: Response) => {
+    try{
+        const { nome, email, idade} = req.body;
+        if(!nome || !email || typeof idade !== "number"){
+            return res.status(400).json({
+                success: false,
+                message: "Dados inválidos"
+            })
+        }
+        const user = await service.create({nome, email, idade})
+        res.status(201).json({
+            success: true,
+            message: "Usuário criado com sucesso",
+            data: user
+        })
+    }
+    catch(error: any){
+        res.status(400).json({
+            success: false,
+            messsage: error.message || "Ocorreu um erro inesperado"
+        })
+    }
+}
+
+export const listUsers = async (req: Request, res: Response) => {
+    const users = await service.list();
+    res.json(200).json({
+        sucess:true,
+        data: users
+    });
+}
+
+export const deleteUser = async(req: Request, res: Response) => {
+    const id = Number(req.params.id);
+    try{
+        await service.remove(id);
+        res.status(204).json({
+            success: true,
+            message: "Usuário removido com sucesso"
+        });
+    }
+    catch(error: any){
+        res.status(400).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
