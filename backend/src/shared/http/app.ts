fd03406f8c97825abb2b64f168c924644d4be70e
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-09 21:20:39 -0400

    fix: configura cors para acesso do frontend

--- backend/src/shared/http/app.ts
+++ backend/src/shared/http/app.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import routes from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 
 // middleware
 app.use(express.json());
+
+app.use(cors({
+    origin: process.env.URL_FRONTEND
+}));
 
 app.use('/api', routes);
 
