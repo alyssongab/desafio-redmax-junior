0bbc000cf745a123d3fe0116b97dfe295158e440
Alysson Gabriel <alysson.gabriel61@gmail.com>
2025-08-10 17:33:17 -0400

    build: altera variaveis de ambiente no docker

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const baseURL = typeof window === 'undefined'
+  ? process.env.API_URL_INTERNAL
+  : process.env.NEXT_PUBLIC_API_URL_EXTERNAL;
+
 export const api = axios.create({
-    baseURL: process.env.API_BASE_URL
+    baseURL: baseURL
 });
